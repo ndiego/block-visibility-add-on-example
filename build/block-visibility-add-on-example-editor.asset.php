3737c8ae68f9c3ddcffa37e7afe4d78dc4f32163
Nick Diego <ndiego@outermost.co>
2023-03-20 20:51:26 -0500

    Fresh build.

--- build/block-visibility-add-on-example-editor.asset.php
+++ build/block-visibility-add-on-example-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '449decbd4bdedd5eb8a3');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b2c582b48078b4360fd5');
--- build/block-visibility-add-on-example-settings.asset.php
+++ build/block-visibility-add-on-example-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '5b0de380293a83c020bd');
+<?php return array('dependencies' => array('wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '613e206db24319495cbb');
